bfe55fae182907d6aef2304127be493551e63a5d
Ariel Adonai Souza <arielrenostro@gmail.com>
2018-05-12 10:59:24 -0300

    Correção na mensagem da exception Nan

--- src/br/furb/aed/trabalho/exception/ErroNaNException.java
+++ src/br/furb/aed/trabalho/exception/ErroNaNException.java
@@ -3,9 +3,9 @@ package br.furb.aed.trabalho.exception;
 public class ErroNaNException extends RuntimeException {
 
 	private static final long serialVersionUID = 6230506398914748877L;
-	
+
 	public ErroNaNException() {
-		super("Oh jovem, não sabe que não pode dividir por zero?");
+		super("Oh jovem, você digitou uma expressão que retorna um NaN!");
 	}
 
 }
